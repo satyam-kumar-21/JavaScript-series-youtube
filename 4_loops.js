298056456318261c8613aae2f163cc07ccf71dee
Satyam kumar <satyamkumar2716@gmail.com>
2026-08-25 11:42:11 +0530

    update

---
+++ 4_loops.js
@@ -0,0 +1,82 @@
+// console.log("Step 1")
+// console.log("Step 2")
+// console.log("Step 3")
+// console.log("Step 4")
+
+
+//loops
+
+// console.log("Satyam")
+// console.log("Satyam")
+// console.log("Satyam")
+// console.log("Satyam")
+// console.log("Satyam")
+// console.log("Satyam")
+// console.log("Satyam")
+// console.log("Satyam")
+// console.log("Satyam")
+// console.log("Satyam")
+
+// for(let i = 0; i <= 100; i++){
+//     console.log("Satyam")
+// }
+
+
+//for loop
+
+// for (let i = 1; i <= 1; i--){
+//     console.log(i)
+// }
+
+
+// for (let i = 1; i <= 20; i++){
+//     if(i%2 == 0) {
+//         console.log(i)
+//     }
+// }
+
+
+// let i = 11
+
+// while(i <= 10){
+//     console.log(i)
+//     i++
+// }
+
+
+// let i = 11
+
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10)
+
+
+
+//break 
+
+// for (let i = 1; i <= 10; i++){
+//     if(i === 5){
+//         break;
+//     }
+//     console.log(i)
+// }
+
+
+//continue
+
+// for (let i = 1; i <= 10; i++){
+//     if(i === 5){
+//        continue;
+//     }
+//     console.log(i)
+// }
+
+
+//nested loop
+
+for(let i = 1; i<=5; i++){
+    for(let j = 1; j<=5; j++){
+        console.log(i,j)
+    }
+}

2c26e71b1746c794b2b3e942268a55dd506e5cee
Satyam kumar <satyamkumar2716@gmail.com>
2026-08-30 12:53:57 +0530

    change

---
+++ lexical_scope.js
@@ -0,0 +1,28 @@
+// let name = "Satyam"
+
+// function outer(){
+//     let age = 23
+//     function inner(){
+//         console.log(age)
+//         console.log(name)
+//     }
+
+//     inner()
+// }
+// outer()
+
+
+
+
+let age = 34
+function outer(){
+
+    function inner(){
+
+        console.log(age)
+
+    }
+
+    inner()
+}
+outer()
